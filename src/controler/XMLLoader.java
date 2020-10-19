e06f0955080b1aa93008a208e1f5642b6256d077
Lucas Tissier <lucastissier51@gmail.com>
2020-10-19 18:15:13 +0200

    Added Request XML loading ( to be tested, specially time)

--- src/controler/XMLLoader.java
+++ src/controler/XMLLoader.java
@@ -1,21 +1,18 @@
 package controler;
 
-import model.Intersection;
-import model.Map;
-
-import java.io.File;
-import java.io.IOException;
+import model.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
-import model.Segment;
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
-
-
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -89,7 +86,7 @@ public class XMLLoader {
     public void parseRequestXML(String pathNameXMLFile, Map map){
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        HashMap<Long, Intersection> mapIntersection = new HashMap<Long, Intersection>();
+        ArrayList<Request> listRequests = new ArrayList<Request>();
 
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
@@ -104,29 +101,32 @@ public class XMLLoader {
             for(int i = 0; i < nbNode; i++){
                 Node n = nodes.item(i);
                 String nodeName = n.getNodeName();
-                ArrayList<Segment> listSegment = new ArrayList<Segment>();
 
                 if(n.getAttributes() != null && n.getAttributes().getLength() > 0){
 
                     NamedNodeMap att = n.getAttributes();
                     int nbAtt = att.getLength();
 
-                    if(nodeName == "intersection")
+                    if(nodeName == "request")
                     {
-                        long id = Long.parseLong(att.item(0).getNodeValue());
-                        double latitude = Double.parseDouble(att.item(1).getNodeValue());
-                        double longitude = Double.parseDouble(att.item(2).getNodeValue());
-                        //Intersection intersection = new Intersection(id, latitude, longitude,listSegment);
+                        long pickUpAdress = Long.parseLong(att.item(0).getNodeValue());
+                        long deliveryAdress = Long.parseLong(att.item(1).getNodeValue());
+                        Time deliveryDuration = Time.valueOf(att.item(2).getNodeValue());
+                        Time pickUpDuration = Time.valueOf(att.item(3).getNodeValue());
+                        Intersection pickupIntersection = map.getListIntersection().get(pickUpAdress);
+                        Intersection deliveryIntersection = map.getListIntersection().get(deliveryAdress);
+                        PickUpPoint pickUpPoint = new PickUpPoint(pickupIntersection,pickUpDuration);
+                        DeliveryPoint deliveryPoint = new DeliveryPoint(deliveryIntersection,deliveryDuration);
+                        Request request = new Request(pickUpPoint,deliveryPoint);
+                        listRequests.add(request);
                     }
 
-                    if(nodeName == "segment")
+                    if(nodeName == "depot")
                     {
-                        long destination = Long.parseLong(att.item(0).getNodeValue());
-                        double length = Double.parseDouble(att.item(1).getNodeValue());
-                        String name = att.item(2).getNodeValue();
-                        long origin = Long.parseLong(att.item(3).getNodeValue());
-                        //Segment segment = new Segment(destination, length, name, origin);
-                        //listSegment.add(segment);
+                        long adress = Long.parseLong(att.item(0).getNodeValue());
+                        Time departureTime = Time.valueOf(att.item(1).getNodeValue());
+                        Depot depot = new Depot(adress, departureTime);
+                        map.setDepot(depot);
                     }
                 }
             }
--- src/model/Map.java
+++ src/model/Map.java
@@ -10,36 +10,36 @@ public class Map extends Observable {
      *
      */
     protected List<Request> listRequests;
-    protected List<Intersection> listIntersections;
+    protected HashMap<Long,Intersection> listIntersections;
+    protected Depot depot;
 
     /**
      * Default constructor
      */
-    protected List<Intersection> listIntersection;
 
     public Map(){
 
     }
 
-    public Map(List<Intersection> listIntersection) {
-        this.listIntersection = listIntersection;
+    public Map(HashMap<Long,Intersection> listIntersection) {
+        this.listIntersections = listIntersection;
     }
 
-    public List<Intersection> getListIntersection() {
-        return listIntersection;
+    public HashMap<Long,Intersection> getListIntersection() {
+        return listIntersections;
     }
 
-    public void setListIntersection(List<Intersection> listIntersection) {
-        this.listIntersection = listIntersection;
+    public void setListIntersection(HashMap<Long,Intersection> listIntersection) {
+        this.listIntersections = listIntersection;
     }
 
 
 
     public void display(){
-        int nbIntersection = listIntersection.size();
+        int nbIntersection = listIntersections.size();
         for(int i = 0; i < nbIntersection; i++)
         {
-            System.out.println(this.listIntersection.get(i));
+            System.out.println(this.listIntersections.get(i));
         }
     }
 
@@ -55,14 +55,21 @@ public class Map extends Observable {
         this.listRequests = listRequests;
     }
 
-    public List<Intersection> getListIntersections() {
+    public HashMap<Long,Intersection> getListIntersections() {
         return listIntersections;
     }
 
-    public void setListIntersections(List<Intersection> listIntersections) {
+    public void setListIntersections(HashMap<Long,Intersection>listIntersections) {
         this.listIntersections = listIntersections;
     }
 
+    public Depot getDepot() {
+        return depot;
+    }
+
+    public void setDepot(Depot depot) {
+        this.depot = depot;
+    }
 
     @Override
     public String toString() {
