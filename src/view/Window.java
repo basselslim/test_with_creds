fe42ad34467662101ddb67a43f40ffd8bb52c296
Armandleullier <armand.leullier@gmail.com>
2020-10-28 18:28:55 +0100

    Fix windows bug

--- src/view/Window.java
+++ src/view/Window.java
@@ -2,38 +2,32 @@ package view;
 
 import controler.Algorithme;
 import controler.Controller;
-import controler.XMLLoader;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import model.Intersection;
 import model.Map;
 import model.Path;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 public class Window extends Application {
 
-    GraphicalView Gview;
-    TextualView Tview = new TextualView();
-    Map map;
-    Controller controller;
 
-    @FXML
-    private Canvas canvas;
+    TextualView Tview = new TextualView();
+    Map map = new Map();
+    GraphicalView Gview;
+    Controller controller = new Controller(map);;
+
+
     @FXML
     private Pane overlay;
     @FXML
@@ -44,8 +38,6 @@ public class Window extends Application {
     @Override
     public void start(Stage MainFrame) throws Exception {
         initUI(MainFrame);
-        map = new Map();
-        controller = new Controller(map);
 
     }
 
@@ -66,58 +58,45 @@ public class Window extends Application {
     }
 
     public void Zoom(ActionEvent event) {
-        Gview.zoom();
-        Gview.reloadMap();
-        //canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());;
+        Gview.Zoom();
 
+    }
 
+    public void UnZoom(ActionEvent event) {
+        Gview.unZoom();
     }
 
     public void LoadMap(ActionEvent event) {
 
-        map = new Map();
-        Gview = new GraphicalView(map,canvas,overlay); //Creation de la vue graphique à partir de la map et de la zone d'affichage
+        Gview = new GraphicalView(map,overlay); //Creation de la vue graphique à partir de la map et de la zone d'affichage
 
-        //Chargement map
-        FileChooser mapFileChooser = new FileChooser();
-        mapFileChooser.setTitle("Load Map");
-        mapFileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("XML", "*.xml"));
-        File mapFile = mapFileChooser.showOpenDialog(((Node)event.getSource()).getScene().getWindow());
-        
-        XMLLoader xmlloader = new XMLLoader();
-        xmlloader.parseMapXML(mapFile.getAbsolutePath(), map);
+        //Load the map
+        controller.LoadMap(event);
 
-        //Dessin map
-        Gview.drawMap();
+        //Draw the map
+        Gview.refreshMap();
 
-        ((Node) event.getSource()).setDisable(true);
+        //reactivate Requests button
         btn_load_requests.setDisable(false);
     }
 
     public void LoadRequests(ActionEvent event) {
-        FileChooser requestsFileChooser = new FileChooser();
-        requestsFileChooser.setTitle("Load Requests");
-        requestsFileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("XML", "*.xml"));
-        File requestsFile = requestsFileChooser.showOpenDialog(((Node)event.getSource()).getScene().getWindow());
+        controller.LoadRequests(event);
 
-        XMLLoader xmlloader = new XMLLoader();
-        xmlloader.parseRequestXML(requestsFile.getAbsolutePath(), map);
-        System.out.println(map.getListRequests().get(0).getDeliveryPoint());
+        Gview.refreshMap();
 
-        Gview.reloadMap();
-        
         Tview.createRequestList(map, myPane);
-        ((Node) event.getSource()).setDisable(true);
     }
 
     public void Compute(ActionEvent event) {
         Algorithme algo = new Algorithme(map);
-        HashMap<Long, HashMap<Long,Path>> mapSmallestPaths = algo.computeSmallestPaths();
-        //map.setMapSmallestPaths(mapSmallestPaths);
-        //Gview.reloadMap();
+        HashMap<Long, List<Path>> mapSmallestPaths = algo.computeSmallestPaths();
+        map.setMapSmallestPaths(mapSmallestPaths);
+        Gview.refreshMap();
 
-        
-        
+
+
+
 
     }
 
