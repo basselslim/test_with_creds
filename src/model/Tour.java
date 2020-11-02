be06c68321cd70e2f9a60d2fd29d2ca744aec62e
Lucas Tissier <lucastissier51@gmail.com>
2020-11-02 10:34:06 +0100

    Road map in process, missing information on intersections

--- src/model/Tour.java
+++ src/model/Tour.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -11,6 +14,7 @@ public class Tour extends Observable {
      */
     protected int tourLength;
     protected List<Path> listPaths;
+    protected String roadMapFilePath;
 
     /**
      * Default constructor
@@ -25,7 +29,6 @@ public class Tour extends Observable {
             this.tourLength += p.getPathLength();
         }
     }
-
     /**
      * Getters - Setters
      */
@@ -35,5 +38,50 @@ public class Tour extends Observable {
 
     public List<Path> getListPaths() {
         return listPaths;
+    }
+
+    public String generateTextForRoadMap() {
+        String totalText = "Roadmap \n\n\n";
+        int i = 0;
+        for(Path p: listPaths) {
+            i++;
+            String PathTitle = "Path n°" + i + ": from intersection " + p.idDeparture + " to intersection " + p.idArrival + ":\n\n";
+            totalText+=PathTitle;
+            int j = 0;
+            for(Segment s: p.getListSegments()) {
+                j++;
+                String SegmentDescription = "   "+ i +"."+j+": Take " + s.getStreetName() + " in direction of " + s.getDestination() + "\n\n";
+                totalText+=SegmentDescription;
+            }
+            totalText+="\n";
+
+        }
+        return totalText;
+    }
+
+    public void generateRoadMap(String path)
+    {
+        try {
+            File roadMap = new File(path);
+            if (roadMap.createNewFile()) {
+                System.out.println("File created: " + roadMap.getName());
+                System.out.println("Absolute path: " + roadMap.getAbsolutePath());
+                this.roadMapFilePath = roadMap.getAbsolutePath();
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        try {
+            FileWriter myWriter = new FileWriter(this.roadMapFilePath);
+            myWriter.write(this.generateTextForRoadMap());
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
--- src/view/Window.java
+++ src/view/Window.java
@@ -14,8 +14,11 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.Map;
 import model.Path;
+import model.Segment;
+import model.Tour;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -53,6 +56,50 @@ public class Window extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
+        double length1 = 1.1;
+        String streetName1 = "Rue du 0 au 1";
+        long destination1 = 1;
+        Segment s1 = new Segment(length1,streetName1,destination1);
+
+        double length2 = 2.2;
+        String streetName2 = "Rue du 1 au 2";
+        long destination2 = 2;
+        Segment s2 = new Segment(length2,streetName2,destination2);
+
+        double length3 = 3.3;
+        String streetName3 = "Rue du 2 au 3";
+        long destination3 = 3;
+        Segment s3 = new Segment(length3,streetName3,destination3);
+
+        double length4 = 4.4;
+        String streetName4 = "Rue du 3 au 4";
+        long destination4 = 4;
+        Segment s4 = new Segment(length4,streetName4,destination4);
+
+        ArrayList<Segment> listSegments1 = new ArrayList<>();
+        ArrayList<Segment> listSegments2 = new ArrayList<>();
+
+        listSegments1.add(s1);
+        listSegments1.add(s2);
+        listSegments2.add(s3);
+        listSegments2.add(s4);
+
+        long idDeparture1 = 0;
+        long idArrival1 =  2;
+        long idDeparture2 = 2;
+        long idArrival2 = 4;
+
+        Path p1 = new Path(listSegments1,idDeparture1,idArrival1);
+        Path p2 = new Path(listSegments2,idDeparture2,idArrival2);
+
+        ArrayList<Path> listPath = new ArrayList<>();
+
+        listPath.add(p1);
+        listPath.add(p2);
+
+        Tour t = new Tour(listPath);
+        t.generateRoadMap("/Users/lucastissier/Desktop/Roadmap");
 
     }
 
