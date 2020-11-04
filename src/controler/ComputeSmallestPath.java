8dad2034dd217cd690b9abb31b1914dc79fd3c11
jkritter <48760362+jkritter@users.noreply.github.com>
2020-11-04 09:32:27 +0100

    TestAlgorithm

--- src/controler/ComputeSmallestPath.java
+++ src/controler/ComputeSmallestPath.java
@@ -34,11 +34,11 @@ public class ComputeSmallestPath {
                 return computedPath;
             }
             List<Segment> listNeighbours = next.getListSegments();
-            for (Segment s: listNeighbours) {
+            for (Segment s : listNeighbours) {
                 long idNextNode = s.getDestination();
                 Intersection neighbour = this.map.getListIntersections().get(idNextNode);
 
-                if (!(closedMap.containsKey(neighbour.getId()) || (openSet.contains(neighbour) &&  neighbour.getRouteScore() < next.getRouteScore() + 1))) {
+                if (!(closedMap.containsKey(neighbour.getId()) || (openSet.contains(neighbour) && neighbour.getRouteScore() < next.getRouteScore() + 1))) {
                     neighbour.setPrevious(next);
                     neighbour.setRouteScore(next.getRouteScore() + 1);
                     neighbour.setEstimatedScore(neighbour.getRouteScore() + computeCost(neighbour, to));
@@ -58,10 +58,10 @@ public class ComputeSmallestPath {
         double lat1 = Math.toRadians(from.getLatitude());
         double lat2 = Math.toRadians(to.getLatitude());
 
-        double a = Math.pow(Math.sin(dLat / 2),2)
-                + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double a = Math.pow(Math.sin(dLat / 2), 2)
+                + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
         double c = 2 * Math.asin(Math.sqrt(a));
-        
+
         return R * c;
     }
 }
